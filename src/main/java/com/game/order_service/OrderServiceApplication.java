8c415040197e6199fd9690c622e8f34184c48672
Samathi Sapumana <63337505+samathi-97@users.noreply.github.com>
2025-08-06 20:17:03 +0530

    Update OrderServiceApplication.java

--- src/main/java/com/game/order_service/OrderServiceApplication.java
+++ src/main/java/com/game/order_service/OrderServiceApplication.java
@@ -2,12 +2,17 @@ package com.game.order_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EntityScan("com.order.orderservice.model")
+@EnableJpaRepositories("com.order.orderservice.repository")
+@ComponentScan("com.order.orderservice")
 public class OrderServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(OrderServiceApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(OrderServiceApplication.class, args);
+    }
 }
