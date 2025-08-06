52023ece4cfd84e91a4a04a0cfd70a368db1a8c4
Samathi Sapumana <63337505+samathi-97@users.noreply.github.com>
2025-08-06 17:52:45 +0530

    Update Order.java

--- src/main/java/com/game/order_service/model/Order.java
+++ src/main/java/com/game/order_service/model/Order.java
@@ -1,37 +1,37 @@
-package com.game.order_service.model;
+package com.order.orderservice.model;
 
 import jakarta.persistence.*;
-
-import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.List;
 
 @Entity
-@Table(name = "orders")
 public class Order {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private Long orderId;
 
     private String customerName;
 
-    private BigDecimal totalPrice;
+    private LocalDateTime orderDate;
 
-    private LocalDateTime createdAt = LocalDateTime.now();
+    private Double totalPrice;
 
     @OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
     private List<OrderItem> items;
 
     // Getters and Setters
-    public Long getId() { return id; }
-    public String getCustomerName() { return customerName; }
-    public BigDecimal getTotalPrice() { return totalPrice; }
-    public LocalDateTime getCreatedAt() { return createdAt; }
-    public List<OrderItem> getItems() { return items; }
+    public Long getOrderId() { return orderId; }
+    public void setOrderId(Long orderId) { this.orderId = orderId; }
 
-    public void setId(Long id) { this.id = id; }
+    public String getCustomerName() { return customerName; }
     public void setCustomerName(String customerName) { this.customerName = customerName; }
-    public void setTotalPrice(BigDecimal totalPrice) { this.totalPrice = totalPrice; }
-    public void setCreatedAt(LocalDateTime createdAt) { this.createdAt = createdAt; }
+
+    public LocalDateTime getOrderDate() { return orderDate; }
+    public void setOrderDate(LocalDateTime orderDate) { this.orderDate = orderDate; }
+
+    public Double getTotalPrice() { return totalPrice; }
+    public void setTotalPrice(Double totalPrice) { this.totalPrice = totalPrice; }
+
+    public List<OrderItem> getItems() { return items; }
     public void setItems(List<OrderItem> items) { this.items = items; }
 }
