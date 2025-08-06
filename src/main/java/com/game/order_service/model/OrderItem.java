47d081a20d7a83a82055dd034451a17e07e364bd
Samathi Sapumana <63337505+samathi-97@users.noreply.github.com>
2025-08-06 17:59:40 +0530

    Update OrderItem.java

--- src/main/java/com/game/order_service/model/OrderItem.java
+++ src/main/java/com/game/order_service/model/OrderItem.java
@@ -17,7 +17,6 @@ public class OrderItem {
     @JoinColumn(name = "order_id")
     private Order order;
 
-    // Getters and Setters
     public Long getId() { return id; }
     public void setId(Long id) { this.id = id; }
 
